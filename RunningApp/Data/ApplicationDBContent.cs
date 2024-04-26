705c8ea0560ad599b64eb240285e43206433b877
Jed Gray <62748148+jedgray36@users.noreply.github.com>
2024-04-26 15:30:31 -0400

    Controllers and View update

--- RunningApp/Data/ApplicationDBContent.cs
+++ RunningApp/Data/ApplicationDBContent.cs
@@ -1,7 +1,17 @@
-﻿namespace RunningApp.Data
-{
-	public class ApplicationDBContent
-	{
+﻿using Microsoft.EntityFrameworkCore;
+using RunningApp.Models;
 
-	}
+namespace RunningApp.Data
+{
+	public class ApplicationDBContent : DbContext
+	{
+        public ApplicationDBContent(DbContextOptions<ApplicationDBContent> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Races> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
 }
--- RunningApp/Models/Address.cs
+++ RunningApp/Models/Address.cs
@@ -10,7 +10,6 @@ namespace RunningApp.Models
 		public string Street { get; set; }
 		public string City { get; set; }
 		public string State { get; set; }
-		public string PostCode { get; set; }
 
 	}
 }
